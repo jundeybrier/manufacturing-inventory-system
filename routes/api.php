0f3a61ed7c638e01bedb392e6435a4fdb4d0c18c
Jundey <jundeybrier>
2025-11-05 13:50:09 +0800

    api for transaction

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register-user', [ClientRegistrationController::class, 'store']);
 Route::post('/sync', [SyncController::class, 'sync']);
+Route::post('/sync/transactions', [\App\Http\Controllers\Api\SyncTransactionsController::class, 'store']);
